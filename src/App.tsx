40aeedde138b18dbe7e6d92248a6508348545e9c
aliksab <alexeysavich1112@gmail.com>
2024-12-14 12:08:57 +0300

    Инициализировал FormikProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
+import { Input } from '@chakra-ui/react'
 import './App.css'
 import { Provider } from './components/ui/provider'
+import { FormikProvider, useFormik } from 'formik'
+import {
+    CheckoutFormValues,
+    validateSchema
+} from './components/validation/yup-schema'
 
 function App() {
-    return <Provider>Добавил Chakra</Provider>
+    const formik = useFormik<CheckoutFormValues>({
+        initialValues: {
+            name: '',
+            address: '',
+            floor: 1,
+            totalFloors: 1,
+            square: 0,
+            livingSquare: 0,
+            kitchenSquare: 0
+        },
+        validationSchema: validateSchema,
+        onSubmit: (values, { resetForm }) => {
+            console.log(values)
+            setTimeout(() => {
+                resetForm()
+            }, 1000 * 2)
+        }
+    })
+    return (
+        <Provider>
+            <FormikProvider value={formik}>
+                <form onSubmit={formik.handleSubmit}>
+                    <Input />
+                </form>
+            </FormikProvider>
+        </Provider>
+    )
 }
 
 export default App
